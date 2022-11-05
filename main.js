50c66bfd28d921c427c3c28b75692519f6bfb8e1
Juan Matheus Viana Brito <juanjd10@live.com>
2022-11-05 02:11:09 -0300

    Nlw

--- main.js
+++ main.js
@@ -23,12 +23,20 @@ function createCard(date, day, games) {
 }
 
 document.querySelector("#cards").innerHTML =
+  createCard("20/11", "Domingo", createGame("Qatar", "13:00", "Ecuador")) +
+  createCard(
+    "21/11",
+    "Segunda",
+    createGame("England", "10:00", "Iran") +
+      createGame("Senegal", "13:00", "Netherlands") +
+      createGame("Unitedstates", "16:00", "Wales")
+  ) +
   createCard(
     "24/11",
     "Quinta",
-    createGame("uruguay", "10:00", "southkorea") +
-      createGame("portugal", "13:00", "ghana") +
-      createGame("brazil", "16:00", "serbia")
+    createGame("Uruguay", "10:00", "Southkorea") +
+      createGame("Portugal", "13:00", "Ghana") +
+      createGame("Brazil", "16:00", "Serbia")
   ) +
-  createCard("28/11", "Segunda", createGame("brazil", "13:00", "switzerland")) +
-  createCard("02/12", "Sexta", createGame("brazil", "16:00", "cameroon"))
+  createCard("28/11", "Segunda", createGame("Brazil", "13:00", "Switzerland")) +
+  createCard("02/12", "Sexta", createGame("Cameroon", "16:00", "Brazil"))
